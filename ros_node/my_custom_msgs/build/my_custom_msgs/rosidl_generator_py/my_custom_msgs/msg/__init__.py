993c94811b0af928faa5334eb91c3734466100e1
Jinsun-Lee <012vision@gmail.com>
2024-12-06 12:36:27 +0900

    clean up

--- ros_node/my_custom_msgs/build/my_custom_msgs/rosidl_generator_py/my_custom_msgs/msg/__init__.py
+++
@@ -1,2 +0,0 @@
-from my_custom_msgs.msg._lidar_data import LidarData  # noqa: F401
-from my_custom_msgs.msg._lidar_datas import LidarDatas  # noqa: F401
--- ros_node/my_custom_msgs/install/my_custom_msgs/share/my_custom_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(my_custom_msgs_IDL_FILES "msg/LidarData.idl;msg/LidarDatas.idl")
-set(my_custom_msgs_INTERFACE_FILES "msg/LidarData.msg;msg/LidarDatas.msg")
